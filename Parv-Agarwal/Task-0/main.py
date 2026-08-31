022b23e86b9d2990ede31b11576231155bb8809d
Parv Agarwal <agarwalparv7207@gmail.com>
2026-08-31 20:19:29 +0530

    Task 0

--- Parv-Agarwal/Task-0/main.py
+++ Parv-Agarwal/Task-0/main.py
@@ -47,10 +47,69 @@ class Team:
 
         for player in self.__squad:
             print(player.name)
-
+            
+    def get_squad(self):
+         return self.__squad
 
 class Auction:
     def __init__(self,players,teams):
         self.players=players
         self.teams=teams
 
+    def start(self):
+        print("=== AUCTION ===")
+
+        for i in range(len(self.players)):
+            player=self.players[i]
+            team=self.teams[i%2]
+            price=player.base_price+i
+            team.buy_player(player,price)
+class Match:
+    def __init__(self,team1,team2):
+        self.team1=team1
+        self.team2=team2
+
+    def play(self):
+        print("=== MATCH ===")
+        score1=0
+
+        for player in self.team1.get_squad():
+            score1+=player.perform()
+        score2=0
+
+        for player in self.team2.get_squad():
+            score2+=player.perform()
+
+        print(self.team1.name,"Score:",score1)
+        print(self.team2.name,"Score:",score2)
+
+        if score1>score2:
+            print(self.team1.name,"wins")
+        elif score2>score1:
+            print(self.team2.name,"wins")
+        else:
+            print("Draw")
+
+team1=Team("Team Titans",100)
+team2=Team("Team CSK",100)
+
+players=[
+    Batsman("Rohit Sharma",2),
+    Bowler("Bumrah",2),
+    AllRounder("Hardik Pandya",2),
+    Batsman("Virat Kohli",2),
+    Bowler("Mohammed Shami",2),
+    AllRounder("Ravindra Jadeja",2),
+    Batsman("Shubman Gill",2),
+    Bowler("Mohammed Siraj",2),
+    AllRounder("Ashwin",2),
+    Batsman("Suryakumar Yadav",2)]
+
+teams=[team1,team2]
+auction=Auction(players,teams)
+auction.start()
+print("=== SQUADS ===")
+team1.show_squad()
+team2.show_squad()
+match=Match(team1,team2)
+match.play()
