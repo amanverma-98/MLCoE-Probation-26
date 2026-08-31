1d6052e22ad6e4d1b52f77ecd7fa3e861e7bc13f
kaushal <dekstop123@outlook.com>
2026-08-31 18:13:41 +0530

    code completed trial first

--- task_0.py
+++ task_0.py
@@ -34,7 +34,7 @@ class allrounder(Player):
         print(f"{self.player} make {run_points} runs and took {wicket_points} wickets ,contributing {points} points")
         return points
 
-class teams:
+class Teams:
     def __init__(self,team_name,budget=100):
         self.team_name=team_name
         self.__budget=budget
@@ -51,6 +51,8 @@ class teams:
         print(f"\n {self.team_name}\nRemaining budget : {self.__budget}")
         for i in self.__squad:
             print(f"\n->{i}")
+    def get_squad(self):
+        return self.__squad
 
 class auction:
     def __init__(self,player_name,teams):
@@ -69,8 +71,61 @@ class auction:
             if not sold:
                 print(f"{Player.name} -> unsold")
 
+class match:
+    def __init__(self,team1,team2):
+        self.team1=team1
+        self.team2=team2
+    def scores(self,team):
+        score=0
+        for player in team.get_squad():
+            score+=player.perform()
+        return score
+    def play(self):
+        print("--match start--")
+        print(f"{self.team1.name} performance")
+        score1=self.scores(self.team1)
+        score2=self.scores(self.team2)
+        print(f"\n{self.team1.name} total : {score1}")
+        print(f"\n{self.team2.name} total : {score2}")
+        if score1>score2:
+            print(f"{self.team1.name} won the match!")
+        elif score1<score2:
+            print(f"{self.team2.name} won the match!")
+        else:
+            print("match is draw!!")
+
+players = [
+
+    batsman("Rohit Sharma", 2),
+    batsman("Virat Kohli", 2),
+    batsman("Shubman Gill", 2),
+    batsman("Rishabh Pant", 2),
+
+    bowler("Jasprit Bumrah", 2),
+    bowler("Mohammed Shami", 2),
+    bowler("Kuldeep Yadav", 2),
+
+    allrounder("Hardik Pandya", 2),
+    allrounder("Ravindra Jadeja", 2),
+    allrounder("Axar Patel", 2),
+
+]
+
+team1 = Teams("Team Titans", 100)
+team2 = Teams("Team Chargers", 100)
+team3 = Teams("Team Warriors", 100)
+
+teams = [team1, team2, team3]
+
+act=auction(players,teams)
+act.bidding()
+
+for i in teams:
+    i.show_squad()
 
 
+mat=match(team1,team2)
+mat.play()
 
 
 
